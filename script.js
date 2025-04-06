8244a8bdd27565325eba3582d0a4da12e802d5eb
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2025-04-07 00:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const num=document.querySelector("#num");
+
+let numberArr=[];
+for(let i=1;i<=20;i++){
+	if(i%3==0 && i%5==0){
+		numberArr.push("FizzBuzz");
+	}else if(i%5==0){
+		numberArr.push("Buzz")
+	}else if(i%3==0){
+		numberArr.push("Fizz")
+	}else{
+		numberArr.push(i);
+	}
+}
+
+numberArr.join("/n");
+numberArr.map((i)=>{
+	let p=document.createElement("p")
+	p.innerText=i;
+	num.append(p)
+})
